1c287196a7e6831392a53f831f6b13ba0cc58d05
aakx000 <aakx0506@gmail.com>
2024-05-04 18:01:19 +0300

    edited tests

--- tests/test_account.cpp
+++ tests/test_account.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 #include "gmock/gmock.h"
 
-#include <Account.h>
+#include "Account.h"
 
 class MockAccount: public Account {
 public:
@@ -15,35 +15,35 @@ public:
 
 TEST(Account, Init) {
   MockAccount ac(12, 100);
-  EXPECT_CALL(ac, GetBalance());
-  EXPECT_CALL(ac, id());
-  EXPECT_EQ(ac.GetBalance(), 100);
-  EXPECT_EQ(ac.id(), 12);
+  // EXPECT_CALL(ac, GetBalance());
+  // EXPECT_CALL(ac, id());
+  EXPECT_EQ(ac.Account::GetBalance(), 100);
+  EXPECT_EQ(ac.Account::id(), 12);
 }
 
 TEST(Account, ChangeBalance) {
   MockAccount ac(12, 100);
-  EXPECT_CALL(ac, ChangeBalance(testing::_)).Times(4);
-  EXPECT_CALL(ac, Lock()).Times(4);
-  EXPECT_CALL(ac, GetBalance()).Times(2);
-  EXPECT_CALL(ac, Unlock());
-  EXPECT_THROW(ac.ChangeBalance(20), std::runtime_error);
-  ac.Lock();
-  ac.ChangeBalance(20);
-  EXPECT_EQ(ac.GetBalance(), 120);
-  ac.ChangeBalance(-30);
-  EXPECT_EQ(ac.GetBalance(), 90);
-  ac.Unlock();
-  EXPECT_THROW(ac.ChangeBalance(20), std::runtime_error);
+  // EXPECT_CALL(ac, ChangeBalance(testing::_)).Times(4);
+  // EXPECT_CALL(ac, Lock()).Times(4);
+  // EXPECT_CALL(ac, GetBalance()).Times(2);
+  // EXPECT_CALL(ac, Unlock());
+  EXPECT_THROW(ac.Account::ChangeBalance(20), std::runtime_error);
+  ac.Account::Lock();
+  ac.Account::ChangeBalance(20);
+  EXPECT_EQ(ac.Account::GetBalance(), 120);
+  ac.Account::ChangeBalance(-30);
+  EXPECT_EQ(ac.Account::GetBalance(), 90);
+  ac.Account::Unlock();
+  EXPECT_THROW(ac.Account::ChangeBalance(20), std::runtime_error);
 }
 
 TEST(Account, Lock) {
   MockAccount ac(12, 100);
-  EXPECT_CALL(ac, Unlock());
-  EXPECT_CALL(ac, Lock()).Times(2);
-  ac.Unlock();
-  ac.Lock();
-  EXPECT_THROW(ac.Lock(), std::runtime_error);
+  // EXPECT_CALL(ac, Unlock());
+  // EXPECT_CALL(ac, Lock()).Times(2);
+  ac.Account::Unlock();
+  ac.Account::Lock();
+  EXPECT_THROW(ac.Account::Lock(), std::runtime_error);
 }
 
 
--- tests/test_transaction.cpp
+++ tests/test_transaction.cpp
@@ -1,8 +1,8 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
-#include <Account.h>
-#include <Transaction.h>
+#include "Account.h"
+#include "Transaction.h"
 
 class MockAccount: public Account {
 public:
@@ -16,7 +16,7 @@ public:
 
 class MockTransaction : public Transaction {
 public:
-  MockTransaction() : Transaction(){};
+  MockTransaction() : Transaction(){}
   MOCK_METHOD3(Make, bool(Account&, Account&, int));
   MOCK_CONST_METHOD0(fee, int());
   MOCK_METHOD1(set_fee, void(int));
@@ -24,44 +24,44 @@ public:
 
 TEST(Transaction, Init) {
   MockTransaction tr;
-  EXPECT_CALL(tr, fee());
-  EXPECT_EQ(tr.fee(), 1);
+ // EXPECT_CALL(tr, fee());
+  EXPECT_EQ(tr.Transaction::fee(), 1);
 }
 
 TEST(Transaction, Make) {
   MockTransaction tr;
   MockAccount ac_from(1, 1000);
   MockAccount ac_to(2, 1000);
-  EXPECT_CALL(tr, set_fee(testing::_)).Times(4);
-  EXPECT_CALL(tr, fee()).Times(2);
-  EXPECT_CALL(tr, Make(testing::_, testing::_, testing::_)).Times(7);
-  EXPECT_CALL(ac_from, Lock()).Times(2);
-  EXPECT_CALL(ac_from, ChangeBalance(testing::_)).Times(2);
-  EXPECT_CALL(ac_from, Unlock()).Times(2);
-  EXPECT_CALL(ac_from, GetBalance()).Times(2);
-  EXPECT_CALL(ac_to, GetBalance()).Times(2);
-  tr.set_fee(10);
-  EXPECT_EQ(tr.fee(), 10);
-  tr.set_fee(1);
-  EXPECT_THROW(tr.Make(ac_from, ac_from, 150), std::logic_error);
-  EXPECT_THROW(tr.Make(ac_from, ac_to, -50), std::invalid_argument);
-  EXPECT_THROW(tr.Make(ac_from, ac_to, 50), std::logic_error);
-  tr.set_fee(50);
-  EXPECT_EQ(tr.Make(ac_from, ac_to, 98), false);
-  tr.set_fee(1);
-  ac_from.Lock();
-  EXPECT_THROW(tr.Make(ac_from, ac_to, 150), std::runtime_error);
-  ac_from.ChangeBalance(-900);
-  ac_from.Unlock();
-  EXPECT_EQ(tr.Make(ac_from, ac_to, 150), false);
-  EXPECT_EQ(ac_from.GetBalance(), 100);
-  EXPECT_EQ(ac_to.GetBalance(), 1000);
-  ac_from.Lock();
-  ac_from.ChangeBalance(900);
-  ac_from.Unlock();
-  EXPECT_EQ(tr.Make(ac_from, ac_to, 150), true);
-  EXPECT_EQ(ac_from.GetBalance(), 1000 - tr.fee() - 150);
-  EXPECT_EQ(ac_to.GetBalance(), 1000 + 150);
+  // EXPECT_CALL(tr, set_fee(testing::_)).Times(4);
+  // EXPECT_CALL(tr, fee()).Times(2);
+  // EXPECT_CALL(tr, Make(testing::_, testing::_, testing::_)).Times(7);
+  // EXPECT_CALL(ac_from, Lock()).Times(2);
+  // EXPECT_CALL(ac_from, ChangeBalance(testing::_)).Times(2);
+  // EXPECT_CALL(ac_from, Unlock()).Times(2);
+  // EXPECT_CALL(ac_from, GetBalance()).Times(2);
+  // EXPECT_CALL(ac_to, GetBalance()).Times(2);
+  tr.Transaction::set_fee(10);
+  EXPECT_EQ(tr.Transaction::fee(), 10);
+  tr.Transaction::set_fee(1);
+  EXPECT_THROW(tr.Transaction::Make(ac_from, ac_from, 150), std::logic_error);
+  EXPECT_THROW(tr.Transaction::Make(ac_from, ac_to, -50), std::invalid_argument);
+  EXPECT_THROW(tr.Transaction::Make(ac_from, ac_to, 50), std::logic_error);
+  tr.Transaction::set_fee(50);
+  EXPECT_EQ(tr.Transaction::Make(ac_from, ac_to, 98), false);
+  tr.Transaction::set_fee(1);
+  ac_from.Account::Lock();
+  EXPECT_THROW(tr.Transaction::Make(ac_from, ac_to, 150), std::runtime_error);
+  ac_from.Account::ChangeBalance(-900);
+  ac_from.Account::Unlock();
+  EXPECT_EQ(tr.Transaction::Make(ac_from, ac_to, 150), false);
+  EXPECT_EQ(ac_from.Account::GetBalance(), 100);
+  EXPECT_EQ(ac_to.Account::GetBalance(), 1000);
+  ac_from.Account::Lock();
+  ac_from.Account::ChangeBalance(900);
+  ac_from.Account::Unlock();
+  EXPECT_EQ(tr.Transaction::Make(ac_from, ac_to, 150), true);
+  EXPECT_EQ(ac_from.Account::GetBalance(), 1000 - tr.Transaction::fee() - 150);
+  EXPECT_EQ(ac_to.Account::GetBalance(), 1150);
 }
   
   
